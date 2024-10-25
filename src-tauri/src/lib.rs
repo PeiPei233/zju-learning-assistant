659087b8abbe5db1b49d6f0c335dba6b4bd4d78f
PeiPei233 <2503585731@qq.com>
2024-10-25 15:52:55 +0800

    chore: add tauri updater dependency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,6 +21,7 @@ use zju_assist::ZjuAssist;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_cli::init())
         .plugin(tauri_plugin_shell::init())
